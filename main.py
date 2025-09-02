7e3f4f6ebb6f56fb0f25677358fd7073014b8ea1
Harsh-612 <hchaturvedi126@gmail.com>
2025-09-02 15:24:27 +0530

    added logging

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
 class Input(BaseModel):
     text:str
 
 app=FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    
+)
 
 @app.post("/")
 def predict(data:Input):
+    print("Processing")
     result=data.text[::-1]
+    print("processed")
     return {"input":data.text,"reverse":result}
 
 
